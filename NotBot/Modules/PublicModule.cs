6e3b8bd68da6f8f294bf0b64e67708633f8f0e3e
NotYorick <yorickguichelaar@gmail.com>
2020-03-12 09:37:25 +0100

    Discord.net setup with ping command

---
+++ NotBot/Modules/PublicModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NotBot.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        public CommandService commandService { get; set; }
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        [Summary("E.g. n!ping")]
+        public Task PingAsync() => ReplyAsync("pong");        
+    }
+}
---
+++ NotBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using NotBot.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NotBot
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			using (var services = ConfigureServices())
+			{
+				var client = services.GetRequiredService<DiscordSocketClient>();
+
+				client.Log += LogAsync;
+				services.GetRequiredService<CommandService>().Log += LogAsync;
+
+				// Tokens should be considered secret data and never hard-coded.
+				// We can read from the environment variable to avoid hardcoding.
+				await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
+				await client.StartAsync();
+
+				await client.SetGameAsync("prefix n!, n!help for commandlist", null, ActivityType.Listening);
+				// Here we initialize the logic required to register our commands.
+				await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+				await Task.Delay(-1);
+			}
+		}
+
+		private Task LogAsync(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		private ServiceProvider ConfigureServices()
+		{
+			return new ServiceCollection()
+				.AddSingleton<DiscordSocketClient>()
+				.AddSingleton<CommandService>()
+				.AddSingleton<CommandHandlingService>()
+				.AddSingleton<HttpClient>()
+				.BuildServiceProvider();
+		}
+	}
+}
